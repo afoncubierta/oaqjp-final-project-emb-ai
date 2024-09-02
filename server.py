12fe3c8331b4d108815a59f116d896d03d4f1c2c
Antonio Foncubierta <antonio.foncubierta@gmail.com>
2024-09-02 10:44:14 -0400

    wip: server

--- server.py
+++ server.py
@@ -3,16 +3,18 @@ Flask application
 """
 
 from EmotionDetection import emotion_detector
-from flask import Flask, request
+from flask import Flask, request, jsonify
+import json
 app = Flask(__name__)
 
-@app.route("/emotionDetector")
+@app.route("/emotionDetector", methods=["POST"])
 def emotion():
-    text_to_analyze = request.args.get("statement")
+    request_data = json.loads(request.data)
+    text_to_analyze = request_data["statement"]
     emotions = emotion_detector(text_to_analyze)
 
     dominant_emotion = emotions.pop("dominant_emotion")
 
     response_content = f"For the given statement, the system response is {emotions}. The dominant emotion is <b>{dominant_emotion}</b>."
-
-    return()
+    
+    return(response_content)
