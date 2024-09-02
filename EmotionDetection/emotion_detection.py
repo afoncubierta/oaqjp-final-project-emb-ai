724b8370a04e5c14f4e37c8f0bd719435ba4e64a
Antonio Foncubierta <antonio.foncubierta@gmail.com>
2024-09-02 07:44:50 -0400

    unit testing

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,11 +7,12 @@ Input json: { "raw_document": { "text": text_to_analyse } }
 
 """
 import requests
+import json
 
 WATSON_NLP_EMOTION_PREDICT_URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 WATSON_NLP_EMOTION_PREDICT_HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
-def emotion_detection(text_to_analyze = "This is a very nice default value"):
+def emotion_detector(text_to_analyze = "This is a very nice default value"):
 
     emotion = None
     input_json = {
@@ -20,8 +21,14 @@ def emotion_detection(text_to_analyze = "This is a very nice default value"):
         }
     }
 
-    response = requests.get(WATSON_NLP_EMOTION_PREDICT_URL, headers= WATSON_NLP_EMOTION_PREDICT_HEADERS, params = input_json)
-    
-    emotion = response["text"]
+    response = requests.post(WATSON_NLP_EMOTION_PREDICT_URL, headers= WATSON_NLP_EMOTION_PREDICT_HEADERS, json = input_json)
 
-    return emotion
+    response_dict = json.loads(response.text)
+    predictions = response_dict.get("emotionPredictions")[0]
+    emotions = predictions.get("emotion")
+
+    dominant_emotion = [emotion_name for emotion_name, emotion_score in emotions.items() if emotion_score == max(emotions.values())][0]
+    emotions["dominant_emotion"] = dominant_emotion
+
+    return emotions
+
